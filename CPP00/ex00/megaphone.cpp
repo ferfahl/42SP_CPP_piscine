3427463c67e851315345edf5dc05ff9e61a2329d
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-08-25 15:30:47 -0300

    cpp00 - ex00: done

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 15:10:17 by feralves          #+#    #+#             */
+/*   Updated: 2023/08/25 15:27:33 by feralves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void  capitalizeWords(char *str){
+  while (*str){
+    std::cout << (char)toupper(*str);
+    str++;
+  }
+}
+
+void  megaMessage(void){
+  std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1)
+    megaMessage();
+  else
+    for (int i = 1; i < argc; i++){
+      capitalizeWords(argv[i]);
+      if (i < argc - 1)
+        std::cout << " ";
+    }
+  std::cout << "\n";
+  return 0;
+}
+
