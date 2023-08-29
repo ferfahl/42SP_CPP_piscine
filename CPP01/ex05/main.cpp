a7838b29a94f01d7d56c1482782a5903c5bcdbb0
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-08-29 16:10:24 -0300

    [ex05]

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,10 +6,20 @@
 /*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 21:15:00 by feralves          #+#    #+#             */
-/*   Updated: 2023/08/28 21:15:15 by feralves         ###   ########.fr       */
+/*   Updated: 2023/08/29 16:05:26 by feralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main()
+int	main(void)
+{
+	Harl	h;
+
+	h.complain("DEBUG");
+	h.complain("INFO");
+	h.complain("WARNING");
+	h.complain("ERROR");
+	h.complain("ERRORI");
+	return (0);
+}
