5dfeee40d0aa90ef75b2c3d101c0c59bb8841b36
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-11-03 17:15:43 -0300

    [Exam] subject added

---
+++ CPP_EXAM/ex00/Warlock.hpp
@@ -0,0 +1,21 @@
+#ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+
+# include <iostream>
+
+class Warlock {
+	private:
+		std::string	_name;
+		std::string	_title;
+		Warlock();
+	public:
+		Warlock(std::string name, std::string title);
+		~Warlock();
+
+		std::string	getName() const;
+		std::string	getTitle() const;
+		void		setTitle(std::string title);
+		void		introduction(void);
+};
+
+#endif
